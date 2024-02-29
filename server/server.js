fd3275e99593d2950eff059a1c5fe8bc77b811fc
Saksham1411 <sakshamsamgarg1411@gmail.com>
2024-02-29 18:29:45 +0530

    backend deploy

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/uploads',express.static('./uploads'));
 app.use(cors({
     credentials:true,
-    origin:'http://localhost:5173'
+    origin:process.env.FRONTEND
 }))
 
 app.use('/',userRoutes);
